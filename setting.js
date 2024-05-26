276fbed34df4f7f4de87ea0c47e088664af34972
Dendi R.S <dendirizkysanjaya@gmail.com>
2024-05-26 15:19:13 +0700

    first touch

---
+++ setting.js
@@ -0,0 +1,14 @@
+// this setting for full page script
+var vioku_setting_full = {
+  exceptionurl: "facebook.com,google.com",
+  idblogSafelink: "8343762614364804921",
+};
+// this setting for in web safelink
+var vioku_settings_safelink = {
+  textTimerOnloading: "Please wait [timer] second",
+  button1Onready: "Get Link",
+  button2Onloading: "Please Wait...",
+  button2Onready: "Continue",
+  timerCountDown1: 5, // 5 second
+  timerCountDown2: 3, // 3 second
+};
